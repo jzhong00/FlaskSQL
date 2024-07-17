e874d46279e89d5947b7b0daffb9bcfc0d64998a
Jason Zhong <108452639+jzhong00@users.noreply.github.com>
2024-07-17 18:30:45 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='FlaskSQL',
-    version='1.0.1',
+    version='1.0.3',
     url='https://github.com/jzhong00/FlaskSQL/',
     license='MIT',
     author='Jason Zhong',
